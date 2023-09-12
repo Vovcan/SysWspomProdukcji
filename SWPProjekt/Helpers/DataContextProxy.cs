592d36cc028bea769b396c72b614e19fc2f7f858
Volodymyr Bondar <s96488@pollub.edu.pl>
2023-09-12 14:22:14 +0200

    połączyłem 2 projekty

---
+++ SWPProjekt/Helpers/DataContextProxy.cs
@@ -0,0 +1,24 @@
+﻿using System.Windows;
+
+namespace SWPProjekt.Helpers
+{
+    public class DataContextProxy : Freezable
+    {
+        #region Overrides of Freezable
+
+        protected override Freezable CreateInstanceCore()
+        {
+            return new DataContextProxy();
+        }
+
+        #endregion
+
+        public object DataSource
+        {
+            get { return (object)GetValue(DataProperty); }
+            set { SetValue(DataProperty, value); }
+        }
+
+        public static readonly DependencyProperty DataProperty = DependencyProperty.Register("DataSource", typeof(object), typeof(DataContextProxy), new UIPropertyMetadata(null));
+    }
+}
