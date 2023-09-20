c783e40dfd38e0a44474156588d518c878904ff2
mat784 <109430450+mat784@users.noreply.github.com>
2023-09-20 18:10:47 +0200

    Modole bazy danych

--- SWPProjekt/Model/ProductionDatabaseContext.cs
+++ SWPProjekt/Model/ProductionDatabaseContext.cs
@@ -50,7 +50,7 @@ public partial class ProductionDatabaseContext : DbContext
     public virtual DbSet<WorkingHour> WorkingHours { get; set; }
 
     protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
-//#warning To protect potentially sensitive information in your connection string, you should move it out of source code. You can avoid scaffolding the connection string by using the Name= syntax to read it from configuration - see https://go.microsoft.com/fwlink/?linkid=2131148. For more guidance on storing connection strings, see http://go.microsoft.com/fwlink/?LinkId=723263.
+#warning To protect potentially sensitive information in your connection string, you should move it out of source code. You can avoid scaffolding the connection string by using the Name= syntax to read it from configuration - see https://go.microsoft.com/fwlink/?linkid=2131148. For more guidance on storing connection strings, see http://go.microsoft.com/fwlink/?LinkId=723263.
         => optionsBuilder.UseMySQL("Server=localhost;User=root;Database=production_database;");
 
     protected override void OnModelCreating(ModelBuilder modelBuilder)
