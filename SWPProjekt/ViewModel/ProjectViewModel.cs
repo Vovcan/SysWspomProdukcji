09d87f13222ec8dde470a4586eb0320235c03dc5
mat784 <109430450+mat784@users.noreply.github.com>
2023-11-06 19:35:19 +0100

    Ekran projektu wyświetla imię autora i listę procesów produkcyjnych

--- SWPProjekt/ViewModel/ProjectViewModel.cs
+++ SWPProjekt/ViewModel/ProjectViewModel.cs
@@ -1,6 +1,8 @@
 ﻿using SWPProjekt.Model;
 using System;
 using System.Collections.Generic;
+using System.Collections.ObjectModel;
+using System.Diagnostics;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
@@ -10,9 +12,24 @@ namespace SWPProjekt.ViewModel
     class ProjectViewModel : BaseViewModel
     {
         public Project CurrentProject { get; set; }
+        public User ProjectOwner { get; set; }
+        public ProductionDatabaseContext db { get; set; }
+        public ObservableCollection<Production>? ProductionList { get; set; }
+
         public ProjectViewModel(Project project)
         {
             CurrentProject=project;
+            try
+            {
+                db = new ProductionDatabaseContext();
+                ProjectOwner = db.Users.Single(u => u.Id == project.Userid);
+                ProductionList = new ObservableCollection<Production>(db.Productions.Where(p => p.Projectid == project.Id).ToList());
+            }
+            catch
+            {
+
+            }
+            
         }
     }
 }
