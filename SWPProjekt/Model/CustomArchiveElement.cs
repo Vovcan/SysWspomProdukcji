5b7e1691044c21dc49acb046d2d510799620e206
Volodymyr Bondar <s96488@pollub.edu.pl>
2023-11-17 13:35:23 +0100

    - dodałem do archiwum sprzedaży

---
+++ SWPProjekt/Model/CustomArchiveElement.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SWPProjekt.Model
+{
+    public class CustomArchiveElement
+    {
+        public int Id { get; set; }
+        public DateTime? Date { get; set; }
+        public string Name { get; set; }
+        public bool Status { get; set; }
+        public float Amount { get; set; }
+        public string Product { get; set; }
+
+        public CustomArchiveElement(int Id, DateTime? Date, string Name, bool Status, float Amount, string Product)
+        {
+            this.Id = Id;
+            this.Date = Date;
+            this.Name = Name;
+            this.Status = Status;
+            this.Amount = Amount;
+            this.Product = Product;
+        }
+
+        public CustomArchiveElement() { }
+    }
+
+}
--- SWPProjekt/ViewModel/ArchiveScreenViewModel.cs
+++ SWPProjekt/ViewModel/ArchiveScreenViewModel.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.Data.Entity.Core.Metadata.Edm;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
@@ -9,14 +10,59 @@ namespace SWPProjekt.ViewModel
 {
     class ArchiveScreenViewModel : BaseViewModel
     {
+        public ProductionDatabaseContext context { get; set; } = new ProductionDatabaseContext();
         public MainViewModel MainModel { get; set; }
-
         public Warehouse CurrentWarehouse { get; set; }
+
+        private List<CustomArchiveElement> _customArchiveElements;
+        public List<CustomArchiveElement> CustomArchiveElements
+        {
+            get
+            {
+                return _customArchiveElements;
+            }
+            set
+            {
+                _customArchiveElements = value;
+                OnPropertyChanged(nameof(CustomArchiveElements));
+            } 
+        }
+
+        public List<Sale> SaleItems { get; set; }
+
+        List<CustomArchiveElement> LocalHelpArchiveElements;
+        public List<CustomArchiveElement> SaleItemsFu()
+        {
+            LocalHelpArchiveElements = new List<CustomArchiveElement>();
+            SaleItems = new List<Sale>();
+            SaleItems.AddRange(context.Sales.Where(sale => sale.Delivery.Warehouseid == CurrentWarehouse.Id));
+            for(int i = 0; i < SaleItems.Count; i++)
+            {
+                CustomArchiveElement a = new CustomArchiveElement(SaleItems[i].Id,
+                                                                  SaleItems[i].DateOfSale,
+                                                                  context.Sales
+                                                                    .Where(sale => sale.Id == SaleItems[i].Id)
+                                                                    .Select(sale => sale.Delivery.Product.Name)
+                                                                    .FirstOrDefault(),
+                                                                  false,
+                                                                  SaleItems[i].Amount,
+                                                                  context.Sales
+                                                                    .Where(sale => sale.Id == SaleItems[i].Id)
+                                                                    .Select(sale => sale.Delivery.Unit.Name)
+                                                                     .FirstOrDefault());
+
+                LocalHelpArchiveElements.Add(a);
+            }
+            CustomArchiveElements = LocalHelpArchiveElements;
+            return LocalHelpArchiveElements;
+        }
 
         public ArchiveScreenViewModel(MainViewModel mainModel, Warehouse CurrentWarehouse)
         {
             MainModel = mainModel;
             this.CurrentWarehouse = CurrentWarehouse;
+            CustomArchiveElements = new List<CustomArchiveElement>();
+            CustomArchiveElements.AddRange(SaleItemsFu());
         }
     }
 }
