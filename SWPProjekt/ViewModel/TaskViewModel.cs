91221065cb6a1251c2725092b211a9f2a47ce5c1
mat784 <109430450+mat784@users.noreply.github.com>
2023-11-20 17:02:45 +0100

    Poprawienie ekranu zadania

--- SWPProjekt/ViewModel/TaskViewModel.cs
+++ SWPProjekt/ViewModel/TaskViewModel.cs
@@ -11,11 +11,12 @@ namespace SWPProjekt.ViewModel
     internal class TaskViewModel : BaseViewModel
     {
         public Model.Task CurrentTask { get; set; }
-        //public ObservableCollection<User> Managers { get; set; }
-        //public ObservableCollection<Model.Task> Tasks { get; set; }
         public ProductionDatabaseContext db { get; set; }
         public Production Production { get; set; }
         public MainViewModel MainModel { get; set; }
+        public ObservableCollection<User> Employees { get; set; }
+        public User Author { get; set; }
+        public User SelectedEmployee { get; set; }
         public TaskViewModel(Model.Task task, MainViewModel mainView, User LoginUser)
         {
             MainModel = mainView;
@@ -24,6 +25,8 @@ namespace SWPProjekt.ViewModel
             {
                 db = new ProductionDatabaseContext();
                 Production = db.Productions.Single(u => u.Id == CurrentTask.Productionid);
+                Author = db.Users.Where(u => u.TaskUsers.Any(p => p.Taskid == CurrentTask.Id) && u.JobTitleid==2).FirstOrDefault();
+                Employees = new ObservableCollection<User>(db.Users.Where(u => u.TaskUsers.Any(p => p.Taskid == CurrentTask.Id)&& u.JobTitleid!=2));
             }
             catch
             {
