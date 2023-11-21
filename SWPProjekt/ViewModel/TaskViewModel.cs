f51ca02dd87664db16b3cbf859dc3c6051bd59cd
mat784 <109430450+mat784@users.noreply.github.com>
2023-11-21 14:22:30 +0100

    Usuwanie pracowników z zadania

--- SWPProjekt/ViewModel/TaskViewModel.cs
+++ SWPProjekt/ViewModel/TaskViewModel.cs
@@ -20,37 +20,47 @@ namespace SWPProjekt.ViewModel
         public ProductionDatabaseContext db { get; set; }
         public Production Production { get; set; }
         public MainViewModel MainModel { get; set; }
-        public ObservableCollection<User> Employees { get; set; }
-        public ObservableCollection<User> AllEmployees { get; set; }
+        public User LoginUser { get; set; }
+        public ObservableCollection<User> Employees { get => employees; set { employees = value; } }
+        public ObservableCollection<User> AllEmployees { get => allEmployees; set { allEmployees = value; PropertyChanged(this, new PropertyChangedEventArgs("AllEmployees")); } }
         public User Author { get; set; }
         public User SelectedEmployee { get; set; }
-        private User addedEmployee;
+        private ObservableCollection<User> allEmployees;
+        private User modifiedEmployee;
+        private ObservableCollection<User> employees;
 
-        public User AddedEmployee
+        public User ModifiedEmployee
         {
-            get { return addedEmployee; }
-            set { addedEmployee = value;
-                AddEmployee(addedEmployee);
+            get { return modifiedEmployee; }
+            set
+            {
+                modifiedEmployee = value;
+                if (modifiedEmployee != null)
+                    ChangeEmployee(modifiedEmployee);
+
             }
         }
 
         public RelayCommand AddCommand { get; set; }
+        public RelayCommand RemoveCommand { get; set; }
         public RelayCommand CloseCommand { get; set; }
+        public bool Removing { get; set; } = false;
         public event PropertyChangedEventHandler PropertyChanged;
         public Visibility Visibility { get => visibility; set { visibility = value; PropertyChanged(this, new PropertyChangedEventArgs("Visibility")); } }
-        public TaskViewModel(Model.Task task, MainViewModel mainView, User LoginUser)
+        public TaskViewModel(Model.Task task, MainViewModel mainView, User loginUser)
         {
             MainModel = mainView;
             CurrentTask = task;
+            LoginUser = loginUser;
+            AddCommand = new RelayCommand(AddList);
+            CloseCommand = new RelayCommand(Close);
+            RemoveCommand = new RelayCommand(RemoveList);
             try
-            {
-                AddCommand = new RelayCommand(Add);
-                CloseCommand = new RelayCommand(Close);
+            {  
                 db = new ProductionDatabaseContext();
                 Production = db.Productions.Single(u => u.Id == CurrentTask.Productionid);
                 Author = db.Users.Where(u => u.TaskUsers.Any(p => p.Taskid == CurrentTask.Id) && u.JobTitleid == 2).FirstOrDefault();
                 Employees = new ObservableCollection<User>(db.Users.Where(u => u.TaskUsers.Any(p => p.Taskid == CurrentTask.Id) && u.JobTitleid != 2));
-                AllEmployees = new ObservableCollection<User>(db.Users.Where(u => !u.TaskUsers.Any(p => p.Taskid == CurrentTask.Id) && (u.JobTitleid == 4 || u.JobTitleid==5)));
             }
             catch
             {
@@ -58,25 +68,48 @@ namespace SWPProjekt.ViewModel
             }
         }
 
-        public void Add(object o)
+        public void AddList(object o)
         {
             Visibility = Visibility.Visible;
+            Removing = false;
+            AllEmployees = new ObservableCollection<User>(db.Users.Where(u => !u.TaskUsers.Any(p => p.Taskid == CurrentTask.Id) && (u.JobTitleid == 4 || u.JobTitleid == 5)));
+        }
+        public void RemoveList(object o)
+        {
+            Visibility = Visibility.Visible;
+            Removing = true;
+            AllEmployees = Employees;
         }
         public void Close(object o)
         {
             Visibility = Visibility.Collapsed;
         }
 
-        public void AddEmployee(User employee)
+        public void ChangeEmployee(User employee)
         {
             try
             {
-                TaskUser taskUser = new TaskUser { Userid = employee.Id, Taskid = CurrentTask.Id };
-                db.Add(taskUser);
-                db.SaveChanges();
-                MainModel.UpdateViewCommand.Execute(new TaskViewModel(CurrentTask, MainModel, MainModel.LoginUser));
+                if (Removing)
+                {
+                    TaskUser taskUser = db.TaskUsers.Single(t => t.Taskid == CurrentTask.Id && t.Userid == employee.Id);
+                    db.Remove(taskUser);
+                    db.SaveChanges();   
+                }
+                else
+                {
+                    TaskUser taskUser = new TaskUser { Userid = employee.Id, Taskid = CurrentTask.Id };
+                    db.Add(taskUser);
+                    db.SaveChanges();
+                }
+                Employees = new ObservableCollection<User>(db.Users.Where(u => u.TaskUsers.Any(p => p.Taskid == CurrentTask.Id) && u.JobTitleid != 2));
+                PropertyChanged(this, new PropertyChangedEventArgs("Employees"));
+                Visibility = Visibility.Collapsed;
             }
-            catch{}   
+            catch (Exception e)
+            {
+                Debug.WriteLine($"{e.Message}");
+            }
         }
+
     }
 }
